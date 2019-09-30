de35ff16a59ae0238d9b3953e206c53a8d00a52f
BlerinaP <blerina1.p@gmail.com>
2019-09-30 12:42:33 +0200

    Moving theme to Github

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// 3rd party packages from NPM
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+// Our modules / classes
+import MobileMenu from './modules/MobileMenu.js';
+import HeroSlider from './modules/HeroSlider.js';
+import GoogleMap from './modules/GoogleMap.js';
+
+
+// Instantiate a new object using our modules/classes
+var mobileMenu = new MobileMenu();
+var heroSlider = new HeroSlider();
+var googleMap = new GoogleMap();
+
+alert("This is workinggg!");
