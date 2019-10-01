128dfaf7e4dbca81d9846beaa03515a7d8ba550a
BlerinaP <blerina1.p@gmail.com>
2019-10-01 18:12:06 +0200

    Managing time in javascript while searching

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -8,32 +8,44 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchOverlayField = $("#search-term");
     this.events();
+      this.isOverlayOpen = false;
+      this.typingTimer;
   }
   //2.events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click",this.closeOverlay.bind(this));
-    $(document).on("keyup",this.keyPressDispatcher.bind(this));
+    $(document).on("keydown",this.keyPressDispatcher.bind(this));
+    this.searchOverlayField.on("keydown" , this.typingLogic.bind(this));
   }
   //3. methods(function,actions)
-    keyPressDispatcher(e){
-      console.log(e.keyCode);
+    typingLogic(){
+      clearTimeout(this.typingTimer);
+      this.typingTimer = setTimeout(function() { console.log("This is a timeout test.");}, 2000);
 
-      if(e.keyCode==83){
+    }
+    keyPressDispatcher(e){
+
+      if(e.keyCode==83 && !this.isOverlayOpen){
          this.openOverlay();
       }
-      if(e.keyCode==27) {
+      if(e.keyCode==27 && this.isOverlayOpen) {
           this.closeOverlay();
       }
     }
   openOverlay(){
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    console.log("Our open method just run");
+    this.isOverlayOpen=true;
   }
   closeOverlay(){
     this.searchOverlay.removeClass("search-overlay--active");
     $("body"). removeClass("body-no-scroll");
+      console.log("Our close method just run");
+      this.isOverlayOpen=false;
   }
 }
 export default Search;
