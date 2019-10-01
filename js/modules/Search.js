9750d4b5a990b37a681edb635b9789d00b215170
BlerinaP <blerina1.p@gmail.com>
2019-10-01 17:35:04 +0200

    Using keyboard to open search overlay(S) or to close it(ESC), through keyCodes.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -14,9 +14,19 @@ class Search {
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click",this.closeOverlay.bind(this));
-
+    $(document).on("keyup",this.keyPressDispatcher.bind(this));
   }
   //3. methods(function,actions)
+    keyPressDispatcher(e){
+      console.log(e.keyCode);
+
+      if(e.keyCode==83){
+         this.openOverlay();
+      }
+      if(e.keyCode==27) {
+          this.closeOverlay();
+      }
+    }
   openOverlay(){
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
