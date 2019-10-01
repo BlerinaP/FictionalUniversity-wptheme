c270cec1ed2262c19fd746f137aa8b612069744e
BlerinaP <blerina1.p@gmail.com>
2019-10-01 17:20:39 +0200

    Removing scroll while you are searching

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -14,13 +14,16 @@ class Search {
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click",this.closeOverlay.bind(this));
+
   }
   //3. methods(function,actions)
   openOverlay(){
     this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
   }
   closeOverlay(){
     this.searchOverlay.removeClass("search-overlay--active");
+    $("body"). removeClass("body-no-scroll");
   }
 }
 export default Search;
