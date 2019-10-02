231b98aaa169743ecda3bae517f8f596aa3ea83e
BlerinaP <blerina1.p@gmail.com>
2019-10-02 12:56:27 +0200

    Focusing text field for search imidiatley without having to use the crusor

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 class Search {
   //1.describe and create/initiate our object
   constructor() {
+      this.addSearchHTML();
     this.resultsDiv = $ ("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -32,7 +33,7 @@ class Search {
                   this.resultsDiv.html('<div class="spinner-loader"></div>');
                   this.isSpinnerVisibile = true;
               }
-              this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+              this.typingTimer = setTimeout(this.getResults.bind(this), 550);
           } else {
               this.resultsDiv.html('');
               this.isSpinnerVisibile = false;
@@ -45,13 +46,14 @@ class Search {
     }
 // Showing "general information" and items that we search. ex: when we search lorem- to show all posts with that content.
      getResults() {
-      $.getJSON('http://localhost:3002/wp-json/wp/v2/posts?search=' + this.searchOverlayField.val(), posts => {
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchOverlayField.val(), posts => {
           this.resultsDiv.html(`
            <h2 class="search-overlay__section-title">General Information</h2>
            ${posts.length ?'<ul class ="link-list min-list">' : '<p> No general information matches that search </p>'}
            ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
            ${posts.length ? '</ul>' : ''}
           `);
+          this.isSpinnerVisibile = false;
       });
      }
      // If we touch S or ESC with codes(83,27) overlay to open or close. But using conditions that "if we touch S in other text area to not show Overlay".
@@ -67,6 +69,8 @@ class Search {
   openOverlay(){
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchOverlayField.val('');
+    setTimeout(()=> this.searchOverlayField.focus(),301);
     console.log("Our open method just run");
     this.isOverlayOpen=true;
   }
@@ -75,6 +79,25 @@ class Search {
     $("body"). removeClass("body-no-scroll");
       console.log("Our close method just run");
       this.isOverlayOpen=false;
+  }
+    addSearchHTML(){
+      $("body").append(`
+      <div class="search-overlay" >
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon"aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?"
+                   id="search-term">
+            <i class="fa fa-window-close search-overlay__close"aria-hidden="true"></i>
+        </div>
+    </div>
+
+    <div class="container">
+        <div id="search-overlay__results"></div>
+    </div>
+
+</div>
+      `);
   }
 }
 export default Search;
