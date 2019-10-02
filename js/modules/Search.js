1144cee9a4c6f1dc82fc788d46795f90a9550cc4
BlerinaP <blerina1.p@gmail.com>
2019-10-02 10:39:14 +0200

    Creating loading spinner icon

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -11,6 +11,8 @@ class Search {
     this.searchOverlayField = $("#search-term");
     this.events();
       this.isOverlayOpen = false;
+      this.isSpinnerVisibile = false;
+      this.previousValue;
       this.typingTimer;
   }
   //2.events
@@ -18,22 +20,37 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click",this.closeOverlay.bind(this));
     $(document).on("keydown",this.keyPressDispatcher.bind(this));
-    this.searchOverlayField.on("keydown" , this.typingLogic.bind(this));
+    this.searchOverlayField.on("keyup" , this.typingLogic.bind(this));
   }
   //3. methods(function,actions)
     typingLogic(){
-      clearTimeout(this.typingTimer);
-      this.resultsDiv.html('<div class="spinner-loader"></div>');
-      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      if(this.searchOverlayField.val() != this.previousValue){
+
+          clearTimeout(this.typingTimer);
+          if (this.searchOverlayField.val()) {
+              if( !this.isSpinnerVisibile){
+                  this.resultsDiv.html('<div class="spinner-loader"></div>');
+                  this.isSpinnerVisibile = true;
+              }
+              this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+          } else {
+              this.resultsDiv.html('');
+              this.isSpinnerVisibile = false;
+          }
+
+      }
+
+      this.previousValue = this.searchOverlayField.val();
 
     }
 
      getResults() {
       this.resultsDiv.html("This is working!");
+      this.isSpinnerVisibile = false;
      }
     keyPressDispatcher(e){
 
-      if(e.keyCode==83 && !this.isOverlayOpen){
+      if(e.keyCode==83 && !this.isOverlayOpen && !$("input , textarea").is(':focus')){
          this.openOverlay();
       }
       if(e.keyCode==27 && this.isOverlayOpen) {
