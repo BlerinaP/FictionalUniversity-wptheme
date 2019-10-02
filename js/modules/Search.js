545e2e0f0042c648c78317c9add3350bc4b1b468
BlerinaP <blerina1.p@gmail.com>
2019-10-02 10:09:21 +0200

    Creating loading spinner icon

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 class Search {
   //1.describe and create/initiate our object
   constructor() {
-
+    this.resultsDiv = $ ("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
@@ -23,9 +23,14 @@ class Search {
   //3. methods(function,actions)
     typingLogic(){
       clearTimeout(this.typingTimer);
-      this.typingTimer = setTimeout(function() { console.log("This is a timeout test.");}, 2000);
+      this.resultsDiv.html('<div class="spinner-loader"></div>');
+      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
 
     }
+
+     getResults() {
+      this.resultsDiv.html("This is working!");
+     }
     keyPressDispatcher(e){
 
       if(e.keyCode==83 && !this.isOverlayOpen){
