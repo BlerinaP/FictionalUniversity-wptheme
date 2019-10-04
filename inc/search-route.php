c1c076864983079e258a2232d616257e3a0d1c01
BlerinaP <blerina1.p@gmail.com>
2019-10-04 12:31:05 +0200

    Working with multiple post types

--- inc/search-route.php
+++ inc/search-route.php
@@ -1,5 +1,5 @@
 <?php
-
+ // creating a rout
 add_action('rest_api_init', 'universityRegisterSearch');
 
 function universityRegisterSearch(){
@@ -8,6 +8,55 @@ function universityRegisterSearch(){
         'callback' =>'universitySearchResults'
     ));
 }
-function universitySearchResults () {
-    return 'Congrats, you created a route';
+// creating our own raw json datas( we write them in php and the wordpress convert them in json)
+function universitySearchResults ($data) {
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post','page','professor','program','campus','event'),
+        's' => sanitize_text_field($data['term'])
+    ));
+
+    $results = array(
+            'generalInfo' => array(),
+            'programs' => array(),
+            'professors' => array(),
+            'events' => array(),
+            'campuses' => array()
+    );
+
+    while($mainQuery -> have_posts()){
+        $mainQuery->the_post();
+        if(get_post_type() == 'post' OR get_post_type() == 'page'){
+            array_push($results['generalInfo'],array(
+                'title' => get_the_title(),
+                'link'=> get_the_permalink()
+            ));
+        }
+        if(get_post_type() == 'professor'){
+            array_push($results['professors'],array(
+                'title' => get_the_title(),
+                'link'=> get_the_permalink()
+            ));
+        }
+        if(get_post_type() == 'program'){
+            array_push($results['programs'],array(
+                'title' => get_the_title(),
+                'link'=> get_the_permalink()
+            ));
+        }
+        if(get_post_type() == 'event'){
+            array_push($results['events'],array(
+                'title' => get_the_title(),
+                'link'=> get_the_permalink()
+            ));
+        }
+        if(get_post_type() == 'campus'){
+            array_push($results['campuses'],array(
+                'title' => get_the_title(),
+                'link'=> get_the_permalink()
+            ));
+        }
+
+    }
+
+    return $results;
 }
