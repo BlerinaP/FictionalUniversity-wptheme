15c29426174f1909f474df62426ce4f5bbb93d93
BlerinaP <blerina1.p@gmail.com>
2019-10-07 11:00:49 +0200

    Modification of search logic that is aware of relationship

--- single-professor.php
+++ single-professor.php
@@ -24,24 +24,28 @@ get_header();
   </div>
 
 
-    <?php
-    $relatedPrograms = get_field('related_programs');
+   ?php
 
-    if($relatedPrograms){
-      echo '<hr class="section-break"</hr>';
-      echo '<h2 class="headline headline--medium">Subjet(s) Taught</h2>';
-      echo '<ul class="link-list min-list">';
-      foreach ($relatedPrograms as $program) { ?>
-       <li> <a href="<?php echo get_the_permalink($program);?>"><?php echo get_the_title($program);?></a></li>
-      <?php }
-      echo '</ul>';
-    }
+        $relatedPrograms = get_field('related_programs');
 
-     ?>
+        if ($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
 
- </div>
+      ?>
 
- <?php }
- get_footer();
+    </div>
 
- ?>
+
+
+  <?php }
+
+  get_footer();
+
+?>
