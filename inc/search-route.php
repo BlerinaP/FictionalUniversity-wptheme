af603cd0fe443a11500696d2a2e68fb0948d5721
BlerinaP <blerina1.p@gmail.com>
2019-10-07 12:14:06 +0200

    Putting results after search,of all programs,professors and events for an certain subject.

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,7 +9,7 @@ function universityRegisterSearch() {
     ));
 }
 
-function universitySearchResults($data) {
+function universitySearchResults($data){
     $mainQuery = new WP_Query(array(
         'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
         's' => sanitize_text_field($data['term'])
@@ -23,7 +23,7 @@ function universitySearchResults($data) {
         'campuses' => array()
     );
 
-    while($mainQuery->have_posts()) {
+    while ($mainQuery->have_posts()) {
         $mainQuery->the_post();
 
         if (get_post_type() == 'post' OR get_post_type() == 'page') {
@@ -39,7 +39,7 @@ function universitySearchResults($data) {
             array_push($results['professors'], array(
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
-                'image' => get_the_post_thumbnail_url (0,'professorLandscape')
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
             ));
         }
 
@@ -62,9 +62,9 @@ function universitySearchResults($data) {
             $eventDate = new DateTime(get_field('event_date'));
             $description = null;
             if (has_excerpt()) {
-            $description = get_the_excerpt();
+                $description = get_the_excerpt();
             } else {
-             $description = wp_trim_words(get_the_content(), 18);
+                $description = wp_trim_words(get_the_content(), 18);
             }
             array_push($results['events'], array(
                 'title' => get_the_title(),
@@ -74,38 +74,52 @@ function universitySearchResults($data) {
                 'description' => $description
             ));
         }
-        if($results['programs']){
-            $programsMetaQuery = array('relation' => 'OR');
 
-            foreach($results['programs'] as $item){
-                array_push($programsMetaQuery , array(
-                    'key' => 'related_programs',
-                    'compare' => 'LIKE',
-                    'value' => '"'. $item['id'] .'"'
-                ));
+    }
+
+    $programRelationshipQuery = new WP_Query(array(
+        'post_type' => array('professor','event'),
+        'meta_query' => array(
+            array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . $results['programs'][0]['id'] . '"'
+            ),
+        )
+    ));
+
+    while ($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+
+        if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+            if (has_excerpt()) {
+                $description = get_the_excerpt();
+            } else {
+                $description = wp_trim_words(get_the_content(), 18);
             }
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => $description
+            ));
         }
 
-        $programRelationshipQuery = new WP_Query(array(
-            'post_type' => 'professor',
-            'meta_query' => $programsMetaQuery
-        ));
-
-        while($programRelationshipQuery->have_posts()){
-         $programRelationshipQuery->the_post();
-
-            if (get_post_type() == 'professor') {
-                array_push($results['professors'], array(
-                    'title' => get_the_title(),
-                    'permalink' => get_the_permalink(),
-                    'image' => get_the_post_thumbnail_url (0,'professorLandscape')
-                ));
-            }
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+            ));
         }
+    }
 
-        $results['professors'] = array_values(array_unique($results['professors'],SORT_REGULAR));
-        }
-
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
 
     return $results;
+
 }
