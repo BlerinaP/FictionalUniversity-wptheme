3db7368d8b01e75f0ca5c93e14c076561f2f1f4e
BlerinaP <blerina1.p@gmail.com>
2019-10-10 11:53:34 +0200

    Being able to update notes in front end, and save them.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -7,6 +7,7 @@ import $ from 'jquery';
     events () {
         $(".delete-note").on("click" , this.deleteNote);
         $(".edit-note").on("click" , this.editNote.bind(this));
+        $(".update-note").on("click" , this.updateNote.bind(this));
     }
 
     //Methods will go here
@@ -49,6 +50,32 @@ import $ from 'jquery';
               console.log(response);
          }
         });
+     }
+
+     updateNote(e) {
+         var thisNote = $(e.target).parents("li");
+
+         var ourUpdatedPost = {
+             'title': thisNote.find(".note-title-field").val(),
+             'content': thisNote.find(".note-body-field").val()
+         }
+         $.ajax({
+             beforeSend: (xhr) => {
+                 xhr.setRequestHeader('X-WP-Nonce',universityData.nonce);
+             },
+             url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+             type: 'POST',
+             data: ourUpdatedPost,
+             success: (response) => {
+                 this.makeNoteReadOnly(thisNote);
+                 console.log("Congrats");
+                 console.log(response);
+             },
+             error: (response) => {
+                 console.log("sorry");
+                 console.log(response);
+             }
+         });
      }
 }
 
